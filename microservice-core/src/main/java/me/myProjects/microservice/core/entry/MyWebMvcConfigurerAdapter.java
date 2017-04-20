9cc7bc930cfaa8a80d2b60f09acdc3ccfa24551b
chendong <chendong@broada.com>
2017-04-20 10:14:22 +0800

    静态资源获取 配置；

--- microservice-core/src/main/java/me/myProjects/microservice/core/entry/MyWebMvcConfigurerAdapter.java
+++ microservice-core/src/main/java/me/myProjects/microservice/core/entry/MyWebMvcConfigurerAdapter.java
@@ -2,12 +2,15 @@ package me.myProjects.microservice.core.entry;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by chendong on 2017/4/20.
  * <p>
  * 静态资源 获取配置
+ * <p>
+ * WebMvcConfigurerAdapter 中存在 一些好用 adepter method
  */
 @Configuration
 public class MyWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
@@ -20,4 +23,14 @@ public class MyWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
         registry.addResourceHandler("/rs/**").addResourceLocations("classpath:/my/");
         super.addResourceHandlers(registry);
     }
+
+    /**
+     * 重定向
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/toLogin", "login");
+        super.addViewControllers(registry);
+    }
+
 }
