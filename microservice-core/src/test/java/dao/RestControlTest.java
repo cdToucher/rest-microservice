1e3f9f4b538125208c09049f027f5e36cdfa8648
chendong <chendong@broada.com>
2017-05-04 09:12:00 +0800

    maven 提交；

--- microservice-core/src/test/java/dao/RestControlTest.java
+++ microservice-core/src/test/java/dao/RestControlTest.java
@@ -1,19 +1,34 @@
 package dao;
 
 import me.myProjects.microservice.core.controller.RestfulController;
+import me.myProjects.microservice.core.dao.UserDao;
+import me.myProjects.microservice.core.entry.ApplicationConfig;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Created by chendong on 2017/4/17.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration("classpath:application.xml")
+@ContextConfiguration(classes = ApplicationConfig.class)
 public class RestControlTest {
 
     @Mock
+    private UserDao userDao;
+
+    @InjectMocks
     private RestfulController controller;
 
+    @Test
+    public void test() {
+
+        controller.restTest();
+    }
 
 
 }
